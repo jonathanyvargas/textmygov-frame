e8a8bf8c29387e76a5e7b02d49b96e41d8193a9f
jonathanyvargas <80233506+jonathanyvargas@users.noreply.github.com>
2023-07-10 10:19:06 -0600

    Update iframe-functions.js

--- iframe-functions.js
+++ iframe-functions.js
@@ -1,16 +1,5 @@
 
 
-function tmgWidgetOpen(){
-
-    var tmgFrame = document.getElementById("tmg-iframe-closed");
-      if (tmgFrame.style.display === "block"){
-        tmgFrame.style.display = "none";
-    } 
-    else {
-    tmgFrame.style.display = "block";
-  }
-} 
-
   function showTmgWidgetCloseButton() { 
     document.getElementById("tmg-widget-close-button").style.display = "inline"; 
 }
