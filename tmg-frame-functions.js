e4cd95c03178765bde0e06780202295adb03ace3
jonathanyvargas <80233506+jonathanyvargas@users.noreply.github.com>
2023-07-10 12:49:47 -0600

    Update tmg-frame-functions.js

--- tmg-frame-functions.js
+++ tmg-frame-functions.js
@@ -22,5 +22,16 @@ function tmgWidgetOpen(){
 
   function showTmgWidgetCloseButton() { 
     document.getElementById("tmg-close-button-green").style.display = "inline"; 
+      document.getElementById("tmg-close-button-green-closed").style.display = "inline"; 
+        document.getElementById("tmg-close-button-green-exit").style.display = "inline"; 
+    document.getElementById("tmg-close-button-grey").style.display = "inline"; 
+      document.getElementById("tmg-close-button-grey-closed").style.display = "inline"; 
+        document.getElementById("tmg-close-button-grey-exit").style.display = "inline"; 
+    document.getElementById("tmg-close-button-white").style.display = "inline"; 
+      document.getElementById("tmg-close-button-white-closed").style.display = "inline"; 
+        document.getElementById("tmg-close-button-white-exit").style.display = "inline"; 
+    document.getElementById("tmg-close-button-black").style.display = "inline"; 
+      document.getElementById("tmg-close-button-black-closed").style.display = "inline"; 
+        document.getElementById("tmg-close-button-black-exit").style.display = "inline"; 
 }
   setTimeout("showTmgWidgetCloseButton()", 1800);
