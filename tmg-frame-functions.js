1db55cedc9b22a504fce939260290268cc2579db
jonathanyvargas <80233506+jonathanyvargas@users.noreply.github.com>
2023-07-10 14:36:06 -0600

    Update tmg-frame-functions.js

--- tmg-frame-functions.js
+++ tmg-frame-functions.js
@@ -21,6 +21,7 @@ function tmgWidgetOpen(){
 } 
 
   function showTmgCloseButton() { 
+    document.getElementById("tmg-close-button-green").style.display = "inline"; 
     document.getElementById("tmg-close-button-grey").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButton()", 1800); 
