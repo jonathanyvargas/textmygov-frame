f782a75a6bebf5af6cfc7a6eeaf9a2ff8bcd6b9c
jonathanyvargas <80233506+jonathanyvargas@users.noreply.github.com>
2023-07-11 10:03:51 -0600

    Update tmg-frame-functions.js

--- tmg-frame-functions.js
+++ tmg-frame-functions.js
@@ -20,6 +20,8 @@ function tmgWidgetOpen(){
   }
 } 
 
+
+
   function showTmgCloseButtonGreen() { 
     document.getElementById("tmg-close-button-green").style.display = "inline"; 
 }
@@ -36,10 +38,22 @@ function tmgWidgetOpen(){
   setTimeout("showTmgCloseButtonGreenExit()", 1800);
 
 
+
   function showTmgCloseButtonGrey() { 
     document.getElementById("tmg-close-button-grey").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButtonGrey()", 1800);
+
+  function showTmgCloseButtonGreyClosed() { 
+    document.getElementById("tmg-close-button-grey-closed").style.display = "inline"; 
+}
+  setTimeout("showTmgCloseButtonGreyClosed()", 1800);
+
+  function showTmgCloseButtonGreyExit() { 
+    document.getElementById("tmg-close-button-grey-exit").style.display = "inline"; 
+}
+  setTimeout("showTmgCloseButtonGreyExit()", 1800);
+
 
 
   function showTmgCloseButtonWhite() { 
@@ -47,10 +61,31 @@ function tmgWidgetOpen(){
 }
   setTimeout("showTmgCloseButtonWhite()", 1800);
 
+  function showTmgCloseButtonWhiteClosed() { 
+    document.getElementById("tmg-close-button-white-closed").style.display = "inline"; 
+}
+  setTimeout("showTmgCloseButtonWhiteClosed()", 1800);
+
+  function showTmgCloseButtonWhiteExit() { 
+    document.getElementById("tmg-close-button-white-exit").style.display = "inline"; 
+}
+  setTimeout("showTmgCloseButtonWhiteExit()", 1800);
+
+
 
   function showTmgCloseButtonBlack() { 
     document.getElementById("tmg-close-button-black").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButtonBlack()", 1800);
+
+  function showTmgCloseButtonBlackClosed() { 
+    document.getElementById("tmg-close-button-black-closed").style.display = "inline"; 
+}
+  setTimeout("showTmgCloseButtonBlackClosed()", 1800);
+
+  function showTmgCloseButtonBlackExit() { 
+    document.getElementById("tmg-close-button-black-exit").style.display = "inline"; 
+}
+  setTimeout("showTmgCloseButtonBlackExit()", 1800);
 
 
