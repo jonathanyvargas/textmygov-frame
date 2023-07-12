af711a4d9708f2578dc94bee8174c5aa7b1b1c1b
jonathanyvargas <80233506+jonathanyvargas@users.noreply.github.com>
2023-07-12 14:59:55 -0600

    Update iframe-functions.js

--- iframe-functions.js
+++ iframe-functions.js
@@ -20,7 +20,7 @@ function tmgWidgetOpen(){
   }
 } 
 
-
+document.addEventListener("DOMContentLoaded", function () {
 
   function showTmgCloseButtonGreen() { 
     document.getElementById("tmg-close-button-green").style.display = "inline"; 
@@ -28,14 +28,11 @@ function tmgWidgetOpen(){
   setTimeout("showTmgCloseButtonGreen()", 1800);
 
 
-document.addEventListener("DOMContentLoaded", function () {
-
   function showTmgCloseButtonGreenClosed() { 
     document.getElementById("tmg-close-button-green-closed").style.display = "inline"; 
-
 }
   setTimeout("showTmgCloseButtonGreenClosed()", 1800);
-});
+
 
   function showTmgCloseButtonGreenExit() { 
     document.getElementById("tmg-close-button-green-exit").style.display = "inline"; 
@@ -43,22 +40,22 @@ document.addEventListener("DOMContentLoaded", function () {
   setTimeout("showTmgCloseButtonGreenExit()", 1800);
 
 
-
   function showTmgCloseButtonGrey() { 
     document.getElementById("tmg-close-button-grey").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButtonGrey()", 1800);
 
+    
   function showTmgCloseButtonGreyClosed() { 
     document.getElementById("tmg-close-button-grey-closed").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButtonGreyClosed()", 1800);
 
+    
   function showTmgCloseButtonGreyExit() { 
     document.getElementById("tmg-close-button-grey-exit").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButtonGreyExit()", 1800);
-
 
 
   function showTmgCloseButtonWhite() { 
@@ -66,16 +63,17 @@ document.addEventListener("DOMContentLoaded", function () {
 }
   setTimeout("showTmgCloseButtonWhite()", 1800);
 
+    
   function showTmgCloseButtonWhiteClosed() { 
     document.getElementById("tmg-close-button-white-closed").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButtonWhiteClosed()", 1800);
 
+    
   function showTmgCloseButtonWhiteExit() { 
     document.getElementById("tmg-close-button-white-exit").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButtonWhiteExit()", 1800);
-
 
 
   function showTmgCloseButtonBlack() { 
@@ -83,14 +81,16 @@ document.addEventListener("DOMContentLoaded", function () {
 }
   setTimeout("showTmgCloseButtonBlack()", 1800);
 
+    
   function showTmgCloseButtonBlackClosed() { 
     document.getElementById("tmg-close-button-black-closed").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButtonBlackClosed()", 1800);
 
+    
   function showTmgCloseButtonBlackExit() { 
     document.getElementById("tmg-close-button-black-exit").style.display = "inline"; 
 }
   setTimeout("showTmgCloseButtonBlackExit()", 1800);
 
-
+});
